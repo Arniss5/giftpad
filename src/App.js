6bd488e8d290c6e9e78dffc4379be09d2de0078c
Arniss5 <86066404+Arniss5@users.noreply.github.com>
2023-02-10 22:25:09 +0000

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './normalize.css'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import WelcomePage from './pages/WelcomePage'
 import Main from './pages/Main'
 import {Route, Routes} from 'react-router-dom'
@@ -8,13 +9,14 @@ import {Route, Routes} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <div className='container'>
         <Header />
+      <div className='container'>
         <Routes>
           <Route exact path='/' element={<WelcomePage />}/>
           <Route exact path='/main' element={<Main />}/>
         </Routes>
       </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from '../components/Logo'
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            <div className="author">Arniss5</div>
+            <div className="socials-container"></div>
+        </footer>
+    )
+}
+
+export default Footer
