76a2b224f8c4c630b331afc4eed6eba4a77ca4b8
Arniss5 <86066404+Arniss5@users.noreply.github.com>
2023-02-13 18:19:00 +0000

    create context for pin

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 const Context = React.createContext()
 
 function ContextProvider(props) {
     
+    const [pin, setPin] = useState("")
+    const [enteredPin, setEnteredPin] = useState("")
+    const [isPinCorrect, setIsPinCorrect] = useState(false)
+    console.log(pin)
+    console.log(enteredPin)
+    console.log(isPinCorrect)
+    
+    useEffect(()=> {
+        if(enteredPin.length === 4) {
+            setIsPinCorrect(true)
+        }
+    }, [enteredPin])
+
     return(
-        <Context.Provider value={{}}>
+        <Context.Provider value={{pin, setPin, enteredPin, setEnteredPin, isPinCorrect, setIsPinCorrect}}>
             {props.children}
         </Context.Provider>
     )
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import { Context } from "../Context";
 import "./WelcomePage.css"
 
 function WelcomePage() {
+    const context = useContext(Context)
+
+    function enterPin(e) {
+        if(context.pin.length < 4) {
+            context.setPin(prevPin => prevPin + e.target.innerText)
+        } else if(context.enteredPin.length < 4) {
+            context.setEnteredPin(prevPin => prevPin + e.target.innerText
+            )       
+        }
+    }
+
     return (
         <div class="panels-container">
             <img 
@@ -12,18 +24,18 @@ function WelcomePage() {
             <div class="panel panel-main">
                 <h1>Thoughful <span>gifts</span> at your finger tips</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam dolores qui repudiandae veritatis iste tempore, velit esse vero nobis.</p>
-                <h2>Enter your PIN:</h2>
+                <h2>{context.pin.length === 4 ? "Enter your PIN:" : "Set your PIN:"}</h2>
                 <div class="pin-container">
-                    <div class="pin-num">1</div>
-                    <div class="pin-num">2</div>
-                    <div class="pin-num">3</div>
-                    <div class="pin-num">4</div>
-                    <div class="pin-num">5</div>
-                    <div class="pin-num">6</div>
-                    <div class="pin-num">7</div>
-                    <div class="pin-num">8</div>
-                    <div class="pin-num">9</div>
-                    <div class="pin-num">0</div>
+                    <div class="pin-num" onClick={enterPin}>1</div>
+                    <div class="pin-num" onClick={enterPin}>2</div>
+                    <div class="pin-num" onClick={enterPin}>3</div>
+                    <div class="pin-num" onClick={enterPin}>4</div>
+                    <div class="pin-num" onClick={enterPin}>5</div>
+                    <div class="pin-num" onClick={enterPin}>6</div>
+                    <div class="pin-num" onClick={enterPin}>7</div>
+                    <div class="pin-num" onClick={enterPin}>8</div>
+                    <div class="pin-num" onClick={enterPin}>9</div>
+                    <div class="pin-num" onClick={enterPin}>0</div>
                 </div>
             </div>
         </div> 
