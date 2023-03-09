cbbc722b60ee0520d0869e04e7a1cdfad2d9adf9
Arniss5 <86066404+Arniss5@users.noreply.github.com>
2023-03-09 20:19:28 +0000

    fix favicon

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom"
 import { ContextProvider } from './Context';
+import Favicon from "react-favicon";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ContextProvider>
         <Router>
+        <Favicon url=""></Favicon>
           <App />
         </Router>
     </ContextProvider>
